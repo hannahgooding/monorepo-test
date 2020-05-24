24c99a4e07ff1f2ba2027ace5717a448932007cb
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-05-24 14:18:57 +0100

    Add cors to server

--- rest-api/server.js
+++ rest-api/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 // const users = require("./handlers/users-handlers");
 const posts = require("./handlers/posts-handlers");
@@ -7,5 +8,6 @@ const server = express();
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
 
 server.use(express.json());
+server.use(cors());
 
 server.get("/posts", posts.getAllPostsHandler);
