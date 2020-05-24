55d233285465938527b5df7f62a0bd1e651f3a8f
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-05-24 16:03:16 +0100

    Absorb index.js into server.js

--- rest-api/server/server.js
+++ rest-api/server/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const PORT = process.env.PORT || 3000;
 // const users = require("./handlers/users-handlers");
 const posts = require("./handlers/posts-handlers");
 
 const server = express();
+
+server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
 
 server.use(express.json());
 server.use(cors());
@@ -14,6 +17,11 @@ server.use((req, res, next) => {
   const error = new Error("Route not found");
   error.status = 404;
   next(error);
+});
+
+process.on("unhandledRejection", (error) => {
+  console.error(error);
+  process.exit(1);
 });
 
 module.exports = server;
